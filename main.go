c6d514ce689cb834e573c3e3fab2eee2fa00fa1a
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-11-07 19:51:56 +0900

    Create auth entry point

--- main.go
+++ main.go
@@ -35,11 +35,49 @@ func main() {
 	r.HandleFunc("/auth/callback", sessionCreateHandler)
 	r.HandleFunc("/logout", logoutHandler)
 
+	r.HandleFunc("/sessions/{id}", authenticationHandler)
+
 	http.Handle("/", r)
 	http.ListenAndServe(":8080", nil)
 }
 
 // handlers
+func sessionCreateHandler(w http.ResponseWriter, r *http.Request) {
+	user, err := gothic.CompleteUserAuth(w, r)
+	if err != nil {
+		panic(err)
+	}
+
+	// call API server to find user
+	// Move to API server - start
+	currentUser := &User{
+		Provider: user.Provider,
+		Uid:      user.UserID,
+		Nickname: user.NickName,
+		ImageUrl: user.AvatarURL,
+	}
+	db.Where("provider = ? AND uid = ?", user.Provider, user.UserID).Find(&currentUser)
+	if db.NewRecord(currentUser) {
+		db.Create(currentUser)
+	}
+	// Move to API server - end
+
+	// create session
+	session_ := &Session{
+		UserID: currentUser.ID,
+	}
+	db.Create(session)
+	session, err := store.Get(r, SessionName)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	session.Values["session_id"] = session_.ID
+	session.Save(r, w)
+
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
 func logoutHandler(w http.ResponseWriter, r *http.Request) {
 	session, err := store.Get(r, SessionName)
 	if err != nil {
@@ -52,32 +90,16 @@ func logoutHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/", http.StatusFound)
 }
 
-func sessionCreateHandler(w http.ResponseWriter, r *http.Request) {
-	user, err := gothic.CompleteUserAuth(w, r)
-	if err != nil {
-		panic(err)
-	}
-	currentUser := &User{
-		Provider: user.Provider,
-		Uid:      user.UserID,
-		Nickname: user.NickName,
-		ImageUrl: user.AvatarURL,
-	}
+func authenticationHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	sessionId := vars["id"]
 
-	db.Where("provider = ? AND uid = ?", user.Provider, user.UserID).Find(&currentUser)
-	if db.NewRecord(currentUser) {
-		db.Create(currentUser)
-	}
+	var session string
+	db.Where("id = ?", sessionId).Find(&session)
 
-	session, err := store.Get(r, SessionName)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	session.Values["user_id"] = currentUser.ID
-	session.Save(r, w)
-
-	http.Redirect(w, r, "/", http.StatusFound)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(session)
 }
 
 func authenticate(w http.ResponseWriter, r *http.Request) {
@@ -100,4 +122,10 @@ type User struct {
 	Uid      string
 	Nickname string
 	ImageUrl string
+}
+
+type Session struct {
+	gorm.Model
+	UserID string
+	// Expires
 }
